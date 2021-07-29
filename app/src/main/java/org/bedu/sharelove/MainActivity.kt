8d049208fdfa9948fb0356f97cc745749ac4d1b6
reisenhead <85323168+reisenhead@users.noreply.github.com>
2021-07-29 12:40:11 -0500

    Cambio de cativity

--- app/src/main/java/org/bedu/sharelove/MainActivity.kt
+++ app/src/main/java/org/bedu/sharelove/MainActivity.kt
@@ -1,11 +1,23 @@
 package org.bedu.sharelove
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var boton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+     boton = findViewById(R.id.crear_cuenta)
+        boton.setOnClickListener( View.OnClickListener() {
+
+            startActivity(Intent(this, CrearCuenta::class.java))
+        });
+}
+
 }
