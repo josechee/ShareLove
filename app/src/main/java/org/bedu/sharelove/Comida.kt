8423e3621e9b56a2db55152e76a6c6a91ff19564
reisenhead <85323168+reisenhead@users.noreply.github.com>
2021-07-29 13:49:53 -0500

    Nuevos Activitys

---
+++ app/src/main/java/org/bedu/sharelove/Comida.kt
@@ -0,0 +1,11 @@
+package org.bedu.sharelove
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Comida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comida)
+    }
+}
