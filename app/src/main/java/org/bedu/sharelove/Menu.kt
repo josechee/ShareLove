37007f39885cdab047ff66a2bf1b5a8369d41c84
reisenhead <85323168+reisenhead@users.noreply.github.com>
2021-07-29 16:06:49 -0500

    Cambios en los activitys

---
+++ app/src/main/java/org/bedu/sharelove/Menu.kt
@@ -0,0 +1,11 @@
+package org.bedu.sharelove
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
---
+++ app/src/main/java/org/bedu/sharelove/RecuperarContrasena.kt
@@ -0,0 +1,11 @@
+package org.bedu.sharelove
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecuperarContrasena : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recuperar_contrasena)
+    }
+}
