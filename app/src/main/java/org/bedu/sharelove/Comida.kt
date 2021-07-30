487a336fcb78d10a3446b50e34e0a80280f38223
josechee <josechee.unp@gmail.com>
2021-07-30 01:14:06 -0500

    Agregando flujo, secciones comida y articulos

---
+++ app/src/main/java/org/bedu/sharelove/Comida.kt
@@ -0,0 +1,33 @@
+package org.bedu.sharelove
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+
+class Comida : AppCompatActivity() {
+
+    private lateinit var botonArticulo: ImageButton
+    private lateinit var botonMenuPrincipal: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comida)
+
+        botonArticulo = findViewById(R.id.imgButtonArticulo)
+        botonMenuPrincipal = findViewById(R.id.buttonComidaMenuPrincipal)
+
+        botonArticulo.setOnClickListener {
+
+            startActivity(Intent(this, Articulos::class.java))
+        }
+
+        botonMenuPrincipal.setOnClickListener {
+
+            startActivity(Intent(this, Menu::class.java))
+        }
+
+    }
+}
--- app/src/main/java/org/bedu/sharelove/MainActivity.kt
+++ app/src/main/java/org/bedu/sharelove/MainActivity.kt
@@ -1,11 +1,34 @@
 package org.bedu.sharelove
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var boton: Button
+    private lateinit var boton2: Button
+    private lateinit var boton3: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        boton = findViewById(R.id.crear_cuenta)
+        boton.setOnClickListener {
+
+            startActivity(Intent(this, CrearCuenta::class.java))
+        }
+        boton2 = findViewById(R.id.buttonIniciarSesion)
+        boton2.setOnClickListener {
+
+            startActivity(Intent(this, Menu::class.java))
+        }
+        boton3 = findViewById(R.id.olvidaste_contraseña)
+        boton3.setOnClickListener {
+
+            startActivity(Intent(this, RecuperarContrasena::class.java))
+        }
     }
+
 }
