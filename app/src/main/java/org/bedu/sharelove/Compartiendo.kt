05361a062783f103a7b1df79e92c1892afd70b12
reisenhead <85323168+reisenhead@users.noreply.github.com>
2021-08-12 18:18:33 -0500

    Se aplica funcionalidad en Iconos App Bar Activity Compartir

--- app/src/main/java/org/bedu/sharelove/Compartiendo.kt
+++ app/src/main/java/org/bedu/sharelove/Compartiendo.kt
@@ -1,8 +1,10 @@
 package org.bedu.sharelove
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 
 class Compartiendo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +15,17 @@ class Compartiendo : AppCompatActivity() {
         val inflater = menuInflater
         inflater.inflate(R.menu.menuaction, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item_contact: MenuItem): Boolean {
+
+        when(item_contact.itemId) {
+            R.id.buttonBack -> startActivity(Intent(this, VistaPrincipal::class.java))
+            R.id.buttonRopa -> startActivity(Intent(this, Articulos::class.java))
+            R.id.buttonFood -> startActivity(Intent(this, Comida::class.java))
+            R.id.buttonShearch -> startActivity(Intent(this, VistaPrincipal::class.java))
+
+        }
+
+        return super.onOptionsItemSelected(item_contact)
     }
 }
