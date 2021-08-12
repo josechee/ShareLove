722ce78fb5986294b25390a33ea6c1713cb1b235
reisenhead <85323168+reisenhead@users.noreply.github.com>
2021-08-12 18:23:45 -0500

    Se aplica funcionalidad en Iconos App Bar Activity Compartir

--- app/src/main/java/org/bedu/sharelove/Articulos.kt
+++ app/src/main/java/org/bedu/sharelove/Articulos.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageButton
 
@@ -35,4 +36,16 @@ class Articulos : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
 
     }
+    override fun onOptionsItemSelected(item_contact: MenuItem): Boolean {
+
+        when(item_contact.itemId) {
+            R.id.buttonBack -> startActivity(Intent(this, VistaPrincipal::class.java))
+            R.id.buttonRopa -> startActivity(Intent(this, Articulos::class.java))
+            R.id.buttonFood -> startActivity(Intent(this, Comida::class.java))
+            R.id.buttonShearch -> startActivity(Intent(this, VistaPrincipal::class.java))
+
+        }
+
+        return super.onOptionsItemSelected(item_contact)
     }
+}
--- app/src/main/java/org/bedu/sharelove/Seleccionar.kt
+++ app/src/main/java/org/bedu/sharelove/Seleccionar.kt
@@ -1,8 +1,10 @@
 package org.bedu.sharelove
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 
 class Seleccionar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +15,18 @@ class Seleccionar : AppCompatActivity() {
         val inflater = menuInflater
         inflater.inflate(R.menu.menuaction, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item_contact: MenuItem): Boolean {
+
+        when(item_contact.itemId) {
+            R.id.buttonBack -> startActivity(Intent(this, VistaPrincipal::class.java))
+            R.id.buttonRopa -> startActivity(Intent(this, Articulos::class.java))
+            R.id.buttonFood -> startActivity(Intent(this, Comida::class.java))
+            R.id.buttonShearch -> startActivity(Intent(this, VistaPrincipal::class.java))
+
+        }
+
+        return super.onOptionsItemSelected(item_contact)
     }
 }
