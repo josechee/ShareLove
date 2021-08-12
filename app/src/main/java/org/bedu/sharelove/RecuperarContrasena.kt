46fd1e1844584a25343d29c7bdfcc103db040703
daniel Colorado <daniel.colorado.940426@gmail.com>
2021-08-12 00:13:50 -0700

    Se agrega funcion a btnRegresarInicio

--- app/src/main/java/org/bedu/sharelove/RecuperarContrasena.kt
+++ app/src/main/java/org/bedu/sharelove/RecuperarContrasena.kt
@@ -1,5 +1,6 @@
 package org.bedu.sharelove
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -14,5 +15,12 @@ class RecuperarContrasena : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recuperar_contrasena)
+
+        btnRecuperar = findViewById(R.id.btnRegresarInicio)
+        btnRecuperar.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
+
+
 }
