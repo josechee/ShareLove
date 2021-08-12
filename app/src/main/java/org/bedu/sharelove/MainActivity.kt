28ad1a493b8cc003517f97abe03558886285c1c9
daniel Colorado <daniel.colorado.940426@gmail.com>
2021-08-11 23:47:13 -0700

    Se realiza validacion de login en MainActivity.kt

--- app/src/main/java/org/bedu/sharelove/MainActivity.kt
+++ app/src/main/java/org/bedu/sharelove/MainActivity.kt
@@ -4,31 +4,62 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var boton: Button
     private lateinit var boton2: Button
     private lateinit var boton3: TextView
+    private lateinit var inputCorreo: TextView
+    private lateinit var inputPass: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        boton = findViewById(R.id.BtnCrearCuenta)
+        inputCorreo = findViewById(R.id.txtCorreo)
+        inputPass = findViewById(R.id.txtPassword)
+
+        boton = findViewById(R.id.btnCrearCuenta)
         boton.setOnClickListener {
 
             startActivity(Intent(this, CrearCuenta::class.java))
         }
-        boton2 = findViewById(R.id.buttonIniciarSesion)
+        boton2 = findViewById(R.id.btnIniciarSesion)
         boton2.setOnClickListener {
-
-            startActivity(Intent(this, VistaPrincipal::class.java))
+            registrar()
         }
-        boton3 = findViewById(R.id.ButtonRecuperarPass)
+        boton3 = findViewById(R.id.btnRecuperarPass)
         boton3.setOnClickListener {
-
             startActivity(Intent(this, RecuperarContrasena::class.java))
         }
     }
+
+    private fun registrar() {
+        val correo: String = inputCorreo.getText().toString()
+        val pass: String = inputPass.getText().toString()
+
+        when {
+            correo.length == 0 && pass.length == 0 -> {
+                Toast.makeText(this,"Los campos no pueden estar vacios", Toast.LENGTH_LONG).show()
+            }
+            pass.length == 0 -> {
+                Toast.makeText(this,"Debes ingresar una contraseña", Toast.LENGTH_LONG).show()
+            }
+            correo.length == 0 -> {
+                Toast.makeText(this,"Debes ingresar un correo", Toast.LENGTH_LONG).show()
+            }
+            correo != "usuario@gmail.com" || pass != "1234" -> {
+                Toast.makeText(this, "Los datos no son validos", Toast.LENGTH_LONG).show()
+            }
+            correo == "usuario@gmail.com" && pass == "1234" -> {
+                Toast.makeText(this,"Hola de nuevo!!", Toast.LENGTH_LONG).show()
+                startActivity(Intent(this, VistaPrincipal::class.java))
+
+            }
+        }
+
+    }
+
 
 }
--- app/src/main/java/org/bedu/sharelove/RecuperarContrasena.kt
+++ app/src/main/java/org/bedu/sharelove/RecuperarContrasena.kt
@@ -2,8 +2,15 @@ package org.bedu.sharelove
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
+
+
+
 
 class RecuperarContrasena : AppCompatActivity() {
+
+    private lateinit var btnRecuperar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recuperar_contrasena)
